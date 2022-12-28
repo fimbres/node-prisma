7ca01acafd474c0172e3afd792faf7da954949e4
Isaac <fimbresisaac23@gmail.com>
2022-12-27 16:49:57 -0800

    adding posts routes to the trpc API

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import { PrismaClient, User } from "@prisma/client";
+import { PrismaClient, User, Post } from "@prisma/client";
 import { initTRPC } from '@trpc/server';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
- 
+
 const trpc = initTRPC.create();
 const prisma = new PrismaClient();
- 
+
 const appRouter = trpc.router({
     getUserById: trpc.procedure.input(value => {
         if (typeof value === 'string') return value;
-        throw new Error(`Invalid input: ${typeof value}`)
+        throw new Error(`Invalid input: ${typeof value}`);
     })
     .query(async request => {
         const input = request.input;
@@ -75,7 +75,7 @@ const appRouter = trpc.router({
     }),
     deleteUser: trpc.procedure.input(value => {
         if (typeof value === 'string') return value;
-        throw new Error(`Invalid input: ${typeof value}`)
+        throw new Error(`Invalid input: ${typeof value}`);
     })
     .mutation(async request => {
         const deletedUser = prisma.user.delete({
@@ -85,6 +85,87 @@ const appRouter = trpc.router({
         });
 
         return deletedUser;
+    }),
+    getAllPosts: trpc.procedure.query(async () => {
+        const posts = prisma.post.findMany();
+
+        return posts;
+    }),
+    getPostById: trpc.procedure.input(value => {
+        if(typeof value === "string") return value;
+        throw new Error(`Invalid input: ${typeof value}`);
+    })
+    .query(async request => {
+        const post = prisma.post.findFirst({
+            where: {
+                id: request.input
+            }
+        });
+
+        return post;
+    }),
+    createPost: trpc.procedure.input(values => {
+        if(typeof values !== 'object'){
+            throw new Error('The new post data must be an object!');
+        }
+
+        if(Object.keys(values as Object).length === 0){
+            throw new Error('The new post data is empty!');   
+        }
+
+        return values as Post;
+    })
+    .mutation(async request => {
+        const data : Post = {
+            title: request.input?.title,
+            id: "",
+            averageReating: 0,
+            rating: 0,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            authorId: request.input?.authorId,
+            favoritedById: null
+        }
+
+        const post = prisma.post.create({
+            data
+        });
+
+        return post;
+    }),
+    updatePost: trpc.procedure.input(values => {
+        if(typeof values !== 'object'){
+            throw new Error('The new post data must be an object!');
+        }
+
+        if(Object.keys(values as Object).length === 0){
+            throw new Error('The new post data is empty!');   
+        }
+
+        return values as Post;
+    })
+    .mutation(async request => {
+        const updatedPost = prisma.post.update({
+            where: {
+                id: request.input?.id
+            },
+            data: request.input
+        });
+
+        return updatedPost;
+    }),
+    deletePost: trpc.procedure.input(value => {
+        if (typeof value === 'string') return value;
+        throw new Error(`Invalid input: ${typeof value}`)
+    })
+    .mutation(async request => {
+        const deletedPost = prisma.post.delete({
+            where: {
+                id: request.input
+            }
+        });
+
+        return deletedPost;
     })
 });
 
